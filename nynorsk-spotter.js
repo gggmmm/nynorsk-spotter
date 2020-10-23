d739cfe83555878f97a892e72f96e9fc6849d00c
giovanni <olimareloojie@gmail.com>
2020-10-23 21:14:10 +0200

    fixed error due to lowerCase conversion. Added words

--- nynorsk-spotter.js
+++ nynorsk-spotter.js
@@ -2,8 +2,8 @@ debug = 0;
 words = ["alvorleg", "argumenta", "arbaidarar",
           "bekymra", "berre", "bileta", "blei", "brannar",
           " dei ", "desse", "dårleg", "droppa", "dukka",
-          " eg ", " eiga ", " ein ", "einaste", " eit ", " eitt ", "eigentleg",
-          "farleg", "festklagar", "fjerna", "fleire", "forbod", "forgifta", "fryktar", "frykteleg", "frakta", " frå ",
+          " eg ", " eiga ", " ein ", "einaste", " eit ", " eitt ", "eigentleg", "eg",
+          "farleg", "festklagar", "fjerna", "fleire", "forbod", "forgifta", "fryktar", "frykteleg", "frakta", " frå ", "forandra",
           "gjere", " gong ", "granskar", "gøymd",
           "hendingar", "hakka", "heilt", "heime", " heim",
           "ikkje",
@@ -15,7 +15,7 @@ words = ["alvorleg", "argumenta", "arbaidarar",
           "omveltingar",
           "regjeringa", "ropar", " rota ", "rutinane",
           "stjerna", "skapar", "saka", "sakna", "saman", "seier", "setja", "sjuke", "skaparane", "skjerpa", "skulane", "skular", "skule", "sommaren", "stansar", "stengde", "studentar",
-          "tidene", "timar", "turistane", "turbinane",
+          "tidene", "timar", "turistane", "turbinane", "tida",
           "utan", "utru",
           "valde", "valte", " veit ", " veka ", " vere ", "velkomen",
           "åtvara"];
@@ -37,10 +37,19 @@ for( var k = 0; k<classes.length; k++){
   wordsFound  = [];
 
   for (var i = 0; i < elements.length; i++) {
+    if(debug==1){
+        // console.log("round i "+i)
+        console.log("current element");
+        console.log(elements[i]);
+    }
+
     atleastonefound = false;
     for(var j=0; j<words.length; j++){
-      var s = elements[i].innerHTML;
-      var res = s.search(words[j].toLocaleLowerCase);
+      if(debug==1)
+        // console.log("round j "+j)
+
+      var s = elements[i].innerHTML.toLocaleLowerCase();
+      var res = s.search(words[j]);
 
       if(res != -1) {
         atleastonefound = true;
@@ -48,7 +57,7 @@ for( var k = 0; k<classes.length; k++){
         wordsFound.push(words[j]);
 
         if(debug==1)
-          console.log(words[j]);
+          console.log("words found in current element "+words[j]);
         
         elements[i].style.border = "5px solid red";
       }
